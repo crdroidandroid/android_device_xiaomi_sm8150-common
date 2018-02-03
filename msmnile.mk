6ce2e2a68fd408e233b79fb8c84940be24d4828b
Wei Wang <wvw@google.com>
2018-02-02 21:32:05 -0800

    sm8150-common: Remove QTI perfd
    Bug: 69270928
    Test: Build and boot device and test camera
    Change-Id: I87b283206f462fb5c8ec9cdd303ae2934fe9bfc4

--- msmnile.mk
+++ msmnile.mk
@@ -304,10 +304,6 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.2.vendor
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr
