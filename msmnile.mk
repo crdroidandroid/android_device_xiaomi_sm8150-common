a5092f270c3d9c9d9927c206cb79490d3253301c
Bowgo Tsai <bowgotsai@google.com>
2019-04-03 16:44:10 +0800

    sm8150-common: Adding GSI keys
    Bug: 112293933
    Test: build and checks the keys are installed into ramdisk
    Change-Id: I216f3900e9511e0929418af050ff964898d0aea1

--- msmnile.mk
+++ msmnile.mk
@@ -10,5 +10,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sm8150-common/sm8150-common-vendor.mk)
