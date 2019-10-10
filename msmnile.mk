0bbdb04d0a6dc75322cd1dd788a447f49458675f
Ryan Mitchell <rtmitchell@google.com>
2019-10-10 10:21:46 -0700

    sm8150-common: Use component-override to configure Bluetooth
    Runtime Resource Overlays (RROs) can no longer change the value of
    resources in manifest when read during PackagerParser.
    
    This change uses component-override to enable the service by default
    on sunfish devices.
    
    Remove usages of profile_supported_* from overlays as they no
    longer configure the enable state of Bluetooth componenets.
    
    Bug: 135048762
    Test:
          adb shell pm query-services \
          com.android.bluetooth/.avrcp.AvrcpTargetService
          adb shell pm query-services \
          com.android.bluetooth/.avrcpcontroller.AvrcpControllerService
          adb shell pm query-services \
          com.android.bluetooth/.ba.BATService
          adb shell pm query-services \
          com.android.bluetooth/.hid.HidDeviceService
          adb shell pm query-services \
          com.android.bluetooth/.hid.HidHostService
          adb shell pm query-services \
          com.android.bluetooth/.sap.SapService
    
    Change-Id: If41df9fdefbcd952f8387451dc137565343e3d75
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -151,6 +151,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml \
+    $(LOCAL_PATH)/configs/component-overrides_qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/component-overrides.xml
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
