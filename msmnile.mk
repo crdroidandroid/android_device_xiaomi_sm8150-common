159c1f487a72549602c2324e6bafeec2ee53daa5
trpsl <trpsl755@gmail.com>
2020-03-24 14:10:26 +0100

    sm8150-common: audio: Add Moto Dolby
    Values taken from OP9 Pro and modified by Sasha (helenius147 on Telegram)
    to work with Moto Dolby.
    
    - Additionally remove dax-default.xml from proprietary-files.txt
    - Improve audio_effects.xml changes
    - Move props from product.prop to vendor.prop
    - Dolby: We have 2 Output channels for Speakers
    - Disable AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP
    - ro.control_privapp_permissions=log -> force
    - Remove media_codecs_dolby_audio.xml, because we are not having backend for this.
    - Remove libswvqe Deps, they are not existing here for MotoDolby
    - Adapt for Enforcing A12.1 Sepolicy
    - Add # Dolby declarations for Sepolicy
    
    Co-authored-by: blankaf <realblankaf@gmail.com>
    Co-authored-by: MB <mb150996@gmail.com>
    Co-authored-by: johnmart19 <johnivan19999@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I02e5c2c8b40ab5128c0f26e059406e892319d4c7

--- msmnile.mk
+++ msmnile.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor
 
+# Dolby
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.4-service.clearkey \
