62a9b5e696a134ab578065a946895fb91b6e10c9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 08:06:36 +0100

    sm8150-common: Initial skeleton
    Change-Id: Ic9773001e798552cc4130665a6454fcdaa4ecc72

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+COMMON_PATH := device/xiaomi/sm8150-common
+
+# Inherit the proprietary files
+include vendor/xiaomi/sm8150-common/BoardConfigVendor.mk
