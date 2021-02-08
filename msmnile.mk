1e8ed8e22f0bf42ab2bcd94a195ecfa87d20ec8e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 14:48:21 +0100

    sm8150-common: Import wifi configs
    * From vayu-user 11 RKQ1.200826.002 V12.0.6.0.RJUMIXM release-keys
    
    Change-Id: I363ddda7a99bd261930300dec147f2b86d909727

--- msmnile.mk
+++ msmnile.mk
@@ -373,6 +373,11 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libnl \
