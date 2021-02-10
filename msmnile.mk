bc373eef77c93bc35ead470d3ff78ad0700e9713
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 21:59:26 +0100

    sm8150-common: Build camera libs
    Change-Id: I9b5d730c54018614c8e28a946a1d234663339b0c

--- msmnile.mk
+++ msmnile.mk
@@ -60,6 +60,21 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
+PRODUCT_PACKAGES += \
+    libcamera2ndk_vendor \
+    libdng_sdk.vendor \
+    libgui_vendor \
+    libstdc++.vendor \
+    vendor.qti.hardware.camera.postproc@1.0.vendor
+
+PRODUCT_PACKAGES += \
+    Snap
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd
