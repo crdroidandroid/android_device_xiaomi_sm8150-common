c55772e41866773a124688f84e7290b6b0b38ac7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-10 22:08:13 +0100

    sm8150-common: Build sensors HIDL
    Change-Id: I6986bce0baeb529afd9e9796360faa9380c5b054

--- msmnile.mk
+++ msmnile.mk
@@ -201,6 +201,12 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
+# Sensors
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
