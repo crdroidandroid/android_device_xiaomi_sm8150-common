746f80005a2111d3a4560719dce84965bd44eccc
Quallenauge <Hamsi2k@freenet.de>
2021-02-24 19:40:28 +0100

    sm8150-common: Exclude CarrierConfig overlays from RRO
    They doesn't work right, because xml attributes where
    stripped down, and leading zeros are removed.
    
    E.g. instead of
     <carrier_config mcc="262" mnc="06">
    the values are modified and get
     <carrier_config mcc="262" mnc="6">
    
    This prevents matching of defined carrier definitions.
    
    Change-Id: I11efe716f9d2ea44bef80c15004d7a8cb71b58b9

--- msmnile.mk
+++ msmnile.mk
@@ -337,6 +337,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
