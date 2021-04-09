4c5b3f4b416d313c6394b2e2df7c3734c092f7b9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-04-09 22:17:10 +0530

    sm8150-common: Configure zram from separate fstab
    Change-Id: I3d093f06b08ad1cc049325d0136b60cab70c29d0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -94,6 +94,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.zram
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.zram
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.nfc.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
