178f94167dfb2629bec17cc912bee2c81a45e883
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    sm8150-common: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -412,6 +412,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    CarrierConfigOverlay \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl \
