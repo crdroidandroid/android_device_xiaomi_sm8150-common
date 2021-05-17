deb4f70b966c365b93528c5a484ec7e22af0e947
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-05-17 14:58:15 +0200

    sm8150-common: Build default Trust HIDL
    Change-Id: Icfecbd97df7adccc51406ae667fbc3cb10865a5a

--- msmnile.mk
+++ msmnile.mk
@@ -369,6 +369,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Trust
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
