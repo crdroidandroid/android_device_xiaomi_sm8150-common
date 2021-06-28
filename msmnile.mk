89689204e0a2c2372bff88bb3269bd56642659d8
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-28 19:24:30 +0200

    sm8150-common: Be sure to build vndservicemanager
    * We need it for our blobs
    
    Change-Id: Ib757b97ee2c7d43fe606225366d729854ab987fc

--- msmnile.mk
+++ msmnile.mk
@@ -381,6 +381,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
+
 # Vibrator
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
