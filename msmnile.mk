a5c744370ce838466167db028cbbc6034db4848b
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-03 22:49:17 +0200

    sm8150-common: Add fpc and goodix keylayouts
    * vayu HALs reports a fake touch every time the fingerprint area is pressed
    
    Change-Id: I8cd014352a05557dd6dde70137313e5e40511d53

--- msmnile.mk
+++ msmnile.mk
@@ -232,6 +232,11 @@ PRODUCT_PACKAGES += \
     libipanat \
     liboffloadhal
 
+# Keylayout
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
+    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msmnile
