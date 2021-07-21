6ac0354761141f65e22e558dca3cb1da7bdf1d51
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-21 23:56:43 +0200

    sm8150-common: Build gralloc.msmnile
    * So it turns out that our camera HAL uses QTI allocator when you open camera, but when you enable torch from QS it uses legacy gralloc1 HAL
    
    Change-Id: I05c164e55c61a9ed4b2807a9cac9430459f2d315

--- msmnile.mk
+++ msmnile.mk
@@ -170,6 +170,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
+    gralloc.msmnile \
     hwcomposer.msmnile \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
