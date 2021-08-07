a4b4433b4cf89087153e8a7ce39bc566161e33c5
kubersharma001 <kubersharma001@gmail.com>
2021-08-07 13:35:17 +0530

    sm8150-common: Import missing keylayout
    These are needed for volume up and down
     buttons on headphones to work properly
    
    Change-Id: I9f339a1e058385250dd2fdc11dc3ef3a8d0a1394

--- msmnile.mk
+++ msmnile.mk
@@ -235,6 +235,7 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
