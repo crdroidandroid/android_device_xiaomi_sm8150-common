f6e17d1ce87b62c849818d30306e358e564c0945
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-15 02:58:51 +0200

    sm8150-common: Update blobs to vayu V12.5.3.0.RJUMIXM
    Change-Id: Ib1497889c57baca09f7b966cfa2cc9a50126d864

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,7 +159,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2021-05-01
+VENDOR_SECURITY_PATCH := 2021-07-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
