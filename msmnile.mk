5d5c576842628351d84adeeed54d350b3d31fa25
Fran P <sweetydesigns@gmail.com>
2021-08-22 19:08:02 +0200

    sm8150-common: Import gpio-keys layout
    * Fix pick up phone call from wired headset
    From: https://github.com/crdroidandroid/android_device_xiaomi_raphael/tree/10.0/keylayout

--- msmnile.mk
+++ msmnile.mk
@@ -254,6 +254,7 @@ PRODUCT_PACKAGES += \
 
 # Keylayout
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/sm8150-tavil-snd-card_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/sm8150-tavil-snd-card_Button_Jack.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
