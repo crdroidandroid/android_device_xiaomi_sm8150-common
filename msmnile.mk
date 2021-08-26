26657cdd4fe04c53c063f287355aab5418129046
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-26 02:48:37 +0200

    sm8150-common: rootdir: Initial libperfmgr init config
    * From coral, heavily stripped down
    
    Change-Id: Ib6846e4f4f23baf56910d2330e26846b99830218

--- msmnile.mk
+++ msmnile.mk
@@ -353,6 +353,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.nfc.rc \
+    init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
