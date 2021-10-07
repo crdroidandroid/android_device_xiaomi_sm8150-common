ab39a64d15b45702a5279a8f007e397cd834efe3
Pavel Dubrova <pashadubrova@gmail.com>
2021-10-07 10:39:12 +0300

    sm8150-common: Provide android.frameworks.sensorservice@1.0 on vendor
    * Used by camera libs.
    
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -164,6 +164,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hidl.memory.block@1.0.vendor \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64
