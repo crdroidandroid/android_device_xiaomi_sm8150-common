287a57b755c926bbee2fd95a8632f1b26360967d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 00:46:51 +0200

    sm8150-common: Uprev clearkey DRM HIDL to 1.4
    Change-Id: Id0895fee70495913f57bc8408ea433660467a7c7
    Signed-off-by: neobuddy89 <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -216,7 +216,7 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.3-service.clearkey \
+    android.hardware.drm@1.4-service.clearkey \
     android.hardware.drm@1.4.vendor
 
 # fastbootd
