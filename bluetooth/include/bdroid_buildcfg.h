2a6a0c157eda0d14c679b141764eef31e2849dce
Pranav Vashi <neobuddy89@gmail.com>
2021-11-02 02:41:18 +0530

    sm8150-common: Switch to QCOM BT
    Change-Id: I07fde33f5dfce4f9c727b340684bddf4b51369c9
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -22,12 +22,20 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+
+#include <cutils/properties.h>
+#include <string.h>
+
+#include "osi/include/osi.h"
+
+#pragma push_macro("PROPERTY_VALUE_MAX")
+
 // Disables read remote device feature
 #define MAX_ACL_CONNECTIONS   16
-#define MAX_L2CAP_CHANNELS    16
+#define MAX_L2CAP_CHANNELS    32
 #define BLE_VND_INCLUDED   TRUE
-// skips conn update at conn completion
-#define BT_CLEAN_TURN_ON_DISABLED 1
-/* Increasing SEPs to 12 from 6 to support SHO/MCast i.e. two streams per codec */
-#define AVDT_NUM_SEPS 12
+#define GATT_MAX_PHY_CHANNEL  10
+
+#pragma pop_macro("PROPERTY_VALUE_MAX")
+
 #endif
