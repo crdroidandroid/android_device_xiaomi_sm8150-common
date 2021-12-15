5a920aeab595395459174afe329c945dc660f33f
kubersharma001 <kubersharma001@gmail.com>
2021-12-15 17:06:48 +0000

    sm8150-common: Don't inherit updatable_apex.mk
    This is because of the following reasons:
    - Many build scripts force disable it anyways by settings OVERRIDE_TARGET_FLATTEN_APEX env flag so why bother rendering differences in official and local builds [1][2].
    - Compressed apex causes issues with OTA on A only devices [3].
    - Compressed apex may cause issues on devices which do not meet kernel requirements [4].
    - Highly anticipated mainline modules update aka Play System updates is limited to OEMs/Pixels only [5], we don't do such things on AOSP; shipping Pixel prebuilt apex components feels somewhat unethical.
    
    Ref:
    [1]: https://github.com/lineageos-infra/build-config/blob/8e435ce7ff7c74097b3500e143380273e099aefb/android/build.sh#L9
    [2]: https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/updatable_apex.mk#L19
    [3]: RecoverySystemService: Failed to reserve space for compressed apex
    [4]: https://source.android.com/devices/tech/ota/apex#kernel-requirements
    [5]: "pushed to end-user devices, either by Google (using the Google Play Store infrastructure) or by the Android partner (using a partner-provided OTA mechanism)." as stated in https://source.android.com/devices/architecture/modular-system#architecture
    
    test: m, boots and ota.
    
    Change-Id: I5b5c2366398791100fa2222ef779ca4d90842211
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -15,9 +15,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
