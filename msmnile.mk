3f64ee44dcf47f8537cc0bf5ca0fa1da041fed12
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-01-09 01:17:47 +0100

    sm8150-common: rootdir: Decommonize init.qcom.sensors.sh
    * It's part of sensors proprietary stack
    
    Change-Id: I38ebcbba921085c2d306f2afffd7bfedab5a5590
    Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -340,7 +340,6 @@ PRODUCT_PACKAGES += \
     init.qcom.coex.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
-    init.qcom.sensors.sh \
     init.qcom.sh \
     init.qcom.usb.sh \
     init.qti.chg_policy.sh \
