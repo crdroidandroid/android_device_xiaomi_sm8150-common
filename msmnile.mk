af5a05e22ad086aa58afd141a7fbd7723c9138f2
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:34:57 +0000

    sm8150-common: Removed audio A2dp from msmnile.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: I300369d36399ffb558b98bafc2478b872d09faba
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -79,7 +79,6 @@ PRODUCT_PACKAGES += \
     android.hardware.soundtrigger@2.2-impl
 
 PRODUCT_PACKAGES += \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.msmnile \
     audio.r_submix.default \
