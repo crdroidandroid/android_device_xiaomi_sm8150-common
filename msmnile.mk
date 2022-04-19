b6894181c03ca6b5b62cc1a21f38b97a87e9a544
Pranav Vashi <neobuddy89@gmail.com>
2022-04-19 11:17:21 +0530

    sm8150-common: Add ANT+ HIDL from sweet
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I38ecf85cb4e8a05ccac23dc104930e76f223b2a6

--- msmnile.mk
+++ msmnile.mk
@@ -149,10 +149,14 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService-Soong \
+    com.dsi.ant@1.0.vendor
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
