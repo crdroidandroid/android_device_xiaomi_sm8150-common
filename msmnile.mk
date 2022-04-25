d6c78037bfde0887f2f8dc58e5f08fb7e3dce119
Pranav Vashi <neobuddy89@gmail.com>
2022-04-26 01:14:00 +0530

    Revert "sm8150-common: Build atrace service"
    This reverts commit 025dba6017ff5c2bbf944f10315bf37857050247.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Ifcf9ab605e3498ddc04d0f00c4ac07055eca6586

--- msmnile.mk
+++ msmnile.mk
@@ -76,10 +76,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_nfc/com.nxp.mifare.xml
 
-# Atrace
-PRODUCT_PACKAGES += \
-    android.hardware.atrace@1.0-service
-
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
