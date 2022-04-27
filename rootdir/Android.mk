e882ee93ee9f4551f0864feb7d0d5f1deda7251c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-27 12:03:48 +0200

    sm8150-common: rootdir: Move ueventd.rc to Android.bp
    Change-Id: I0cc19a9870f844809fd008e905a528626fe221b6

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,8 +2,6 @@ LOCAL_PATH := $(call my-dir)
 
 # TODO on S
 # - Move to Soong with prebuilt_root
-# - ueventd.rc can go to vendor/etc/ueventd.rc
-# See https://android-review.googlesource.com/c/platform/system/core/+/1526382
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.recovery.qcom.rc
@@ -11,13 +9,4 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.recovery.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := ueventd.qcom.rc
-LOCAL_MODULE_STEM  := ueventd.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
 include $(BUILD_PREBUILT)
