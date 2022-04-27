7c75bc5632180ce708f1e6626d64bf27d4e3e617
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-04-27 12:54:02 +0200

    sm8150-common: Build IFAAService
    Change-Id: Id69442a4711f5f909ee260a33b30df8270b5c6d2

--- msmnile.mk
+++ msmnile.mk
@@ -232,6 +232,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/permissions/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml
 
+# IFAA manager
+PRODUCT_PACKAGES += \
+    IFAAService
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
