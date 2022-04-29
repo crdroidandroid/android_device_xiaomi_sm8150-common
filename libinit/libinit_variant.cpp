27a6ef13e1512d29989b8e882e8b234429f6a710
Pranav Vashi <neobuddy89@gmail.com>
2022-04-30 03:27:53 +0530

    sm8150-common: init: Set new board prop
    * "ro.product.vendor.cert=M2102J20SG" is new prop found in latest dumps.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iaa72f686460835b9cb3bfd7b7f068b7b8af27dd3

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -33,6 +33,7 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("name", variant.device, true);
     set_ro_build_prop("marketname", variant.marketname, true);
     set_ro_build_prop("model", variant.model, true);
+    set_ro_build_prop("cert", variant.model, true);
     set_ro_build_prop("product", variant.model, false);
     set_ro_build_prop("fingerprint", variant.build_fingerprint, false);
     set_ro_build_prop("description", fingerprint_to_description(variant.build_fingerprint), false);
