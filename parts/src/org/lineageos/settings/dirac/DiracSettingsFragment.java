a5f678695143ad04f5b7f8ec3220b75c829c7c38
Pranav Vashi <neobuddy89@gmail.com>
2022-05-21 15:34:00 +0530

    sm8150-common: parts: Enable hi-fi mode regardless headset and preset
    * Testing manually it seems hi-fi mode works regardless headset and preset value.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I01b8d868d0fd52054e30c41819fb507bf62c14ab
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
+++ parts/src/org/lineageos/settings/dirac/DiracSettingsFragment.java
@@ -75,14 +75,13 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mHifi = (SwitchPreference) findPreference(PREF_HIFI);
         mHifi.setOnPreferenceChangeListener(this);
 
-        boolean hifiEnable = mDiracUtils != null ? mDiracUtils.getHifiMode() : false;
-        mHeadsetType.setEnabled(!hifiEnable && enhancerEnabled);
-        mPreset.setEnabled(!hifiEnable && enhancerEnabled);
-        mHifi.setEnabled(mDiracUtils != null && enhancerEnabled);
-
         mScenes = (ListPreference) findPreference(PREF_SCENE);
         mScenes.setOnPreferenceChangeListener(this);
-        mScenes.setEnabled(mDiracUtils != null && enhancerEnabled);
+
+        mHeadsetType.setEnabled(enhancerEnabled);
+        mPreset.setEnabled(enhancerEnabled);
+        mHifi.setEnabled(enhancerEnabled);
+        mScenes.setEnabled(enhancerEnabled);
     }
 
     @Override
@@ -124,10 +123,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
                 return true;
             case PREF_HIFI:
                 mDiracUtils.setHifiMode((Boolean) newValue ? 1 : 0);
-                if (mDiracUtils.isDiracEnabled()) {
-                    mHeadsetType.setEnabled(!(Boolean) newValue);
-                    mPreset.setEnabled(!(Boolean) newValue);
-                }
                 return true;
             case PREF_PRESET:
                 mDiracUtils.setLevel((String) newValue);
@@ -151,11 +146,6 @@ public class DiracSettingsFragment extends PreferenceFragment implements
         mHeadsetType.setEnabled(isChecked);
         mPreset.setEnabled(isChecked);
         mScenes.setEnabled(isChecked);
-
-        if (!isChecked){
-            mHifi.setChecked(false);
-            mDiracUtils.setHifiMode(0);
-        }
     }
 
     @Override
