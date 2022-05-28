83f1c15d2de1c7fbe533004e96b2ef5ab1799d94
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 11:17:51 +0200

    sm8150-common: Drop init.qcom.coex.sh
    * Unused
    
    Change-Id: Ife6d572f91a75b66a50e71e8669f776ed62f990e

--- msmnile.mk
+++ msmnile.mk
@@ -352,7 +352,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.class_core.sh \
-    init.qcom.coex.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.sh \
