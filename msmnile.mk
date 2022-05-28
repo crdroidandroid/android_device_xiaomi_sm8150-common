e4eb1df837276b1c1195e0530192a1ccc2f28230
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-05-28 10:18:55 +0200

    sm8150-common: Drop QTI camera device usage
    * qcom-camera topic hasn't been ported to 19.1
    * Keep building vendor.qti.hardware.camera.device@1.0 interface lib, IMS stack and possibly camera HAL still needs it
    
    Change-Id: I87bcd5b54ad986d5844df50de243fc1a18507198

--- msmnile.mk
+++ msmnile.mk
@@ -146,6 +146,7 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libgui_vendor \
     libstdc++.vendor \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor
 
 # Consumer IR
