674300a8b2f71a1ebd3e03f4adb6011f95cf37df
Pranav Vashi <neobuddy89@gmail.com>
2022-05-28 09:56:26 +0530

    sm8150-common: audio: Import dolby codecs
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Iccbc8553fd3e93215cf06bc4eed0e004228d8231

--- msmnile.mk
+++ msmnile.mk
@@ -210,7 +210,8 @@ PRODUCT_PACKAGES += \
 
 # Dolby
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
 
 TARGET_EXCLUDES_AUDIOFX := true
 
