672bc137c4981e7a53511ee9835ae69dc4519955
Pranav Vashi <neobuddy89@gmail.com>
2022-06-14 10:09:32 +0530

    sm8150-common: Move props to ignore selinux permission check fail
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: If42796db681cb6d8cf27593c85d06d50e0a6d7e4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -186,7 +186,7 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
 TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
-TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 # Recovery
