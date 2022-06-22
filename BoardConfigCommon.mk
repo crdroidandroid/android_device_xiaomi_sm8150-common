3128fc53bbc47c0b39de0eb0e535a2a6abdcbdc9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-22 16:52:41 +0200

    sm8150-common: Use compressed kernel for legacy devices
    * Unfortunately they have a 64MB recovery partition, which is not enough for uncompressed kernel
    
    Change-Id: I9abd16c8348a0669a3045604c8138b40b0ef6d74

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,7 +88,11 @@ BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 msm_rtb.filter=0x237 service_locator.enable=1 swiotlb=2048 loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+ifeq ($(TARGET_USE_DYNAMIC_PARTITIONS),true)
 BOARD_KERNEL_IMAGE_NAME := Image
+else
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+endif
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
