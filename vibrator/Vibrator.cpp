dotOS-Devices/device_xiaomi_sm8150-common

sm8150-common: vibrator: Rebrand to Xiaomi SM8150
Change-Id: Icd0ec7c857574caec085751253fafa39be065d0d

vibrator/Vibrator.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.vibrator"
+#define LOG_TAG "vendor.qti.vibrator.xiaomi_msmnile"
 
 #include <cutils/properties.h>
 #include <dirent.h>

vibrator/service.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.hardware.vibrator.service"
+#define LOG_TAG "vendor.qti.hardware.vibrator.service.xiaomi_msmnile"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>

crdroidandroid/android_device_xiaomi_sm8150-common

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

vibrator/Vibrator.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.vibrator"
+#define LOG_TAG "vendor.qti.vibrator.xiaomi_sm8150"
 
 #include <cutils/properties.h>
 #include <dirent.h>

vibrator/service.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "vendor.qti.hardware.vibrator.service"
+#define LOG_TAG "vendor.qti.hardware.vibrator.service.xiaomi_sm8150"
 
 #include <android-base/logging.h>
 #include <android/binder_manager.h>