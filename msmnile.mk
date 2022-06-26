dotOS-Devices/device_xiaomi_sm8150-common

sm8150-common: Build custom QTI vibrator AIDL
Change-Id: Ica7bd62e1328bd2d3a0278c6ee3901bddb2cb95d

msmnile.mk
@@ -436,7 +436,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_msmnile
 
 # WiFi
 PRODUCT_PACKAGES += \

crdroidandroid/android_device_xiaomi_sm8150-common

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

msmnile.mk
@@ -466,7 +466,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
+    vendor.qti.hardware.vibrator.service.xiaomi_sm8150
 
 # VNDK
 PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := strict