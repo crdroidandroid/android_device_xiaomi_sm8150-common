4c7f948d9e4ac3f90456ddfe169f85df022bc2f7
Jake Weinstein <jake@aospa.co>
2022-08-27 17:21:04 +0900

    sm8150-common: rootdir: Move background cpuset to CPU0-1
    Background tasks are not UX-critical.
    Free up cores to work on more important
    UX tasks.
    
    Change-Id: Ib0c8671ae56f377e78ba2d07e1a960489496c628
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -65,7 +65,7 @@ case "$target" in
 	echo 1 > /proc/sys/kernel/sched_walt_rotate_big_tasks
 
 	# cpuset parameters
-	echo 0-2     > /dev/cpuset/background/cpus
+	echo 0-1     > /dev/cpuset/background/cpus
 	echo 0-3     > /dev/cpuset/system-background/cpus
 	echo 4-7     > /dev/cpuset/foreground/boost/cpus
 	echo 0-2,4-7 > /dev/cpuset/foreground/cpus
