b479b8b9b988b2e888ce4ef0494a208aaff18b74
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-24 23:30:29 +0100

    sm8150-common: Drop SDM LiveDisplay service
    * We switched to AOSP colors. This conflicts with AOSP colors.
    
    Change-Id: Ia22c4c06e922d1b7f9c60b257299634333cc3fe4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -296,10 +296,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
