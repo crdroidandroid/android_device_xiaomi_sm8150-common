2000802fca369fc5574f8e996b83ce01beb63521
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-11-24 23:30:29 +0100

    sm8150-common: Drop SDM LiveDisplay service
    * We switched to AOSP colors so messing with color calibration will
      confuse QDCM
    
    Change-Id: Ia22c4c06e922d1b7f9c60b257299634333cc3fe4

--- msmnile.mk
+++ msmnile.mk
@@ -244,10 +244,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.xiaomi
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
