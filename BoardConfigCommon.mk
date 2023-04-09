9bca2e69db6177d236ae4f7a197ca1c4b5391a61
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-04-09 21:34:22 +0200

    sm8150-common: Update blobs from vayu V14.0.1.0.TJUMIXM
    Change-Id: I1899096842872ac178d647cacf948aedb4e83abe

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -182,7 +182,7 @@ SOONG_CONFIG_XIAOMI_MSMNILE_ROOTDIR_PARTITION_SCHEME := legacy
 endif
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2022-07-01
+VENDOR_SECURITY_PATCH := 2023-02-01
 
 # Sepolicy
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
