9049f639a7784683551813eeff3386dfdab158b0
Pranav Vashi <neobuddy89@gmail.com>
2023-05-06 00:57:16 +0530

    sm8150-common: rootdir: Remove non-existing cpuset for foreground boost cpus
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -67,7 +67,6 @@ case "$target" in
 	# cpuset parameters
 	echo 0-1     > /dev/cpuset/background/cpus
 	echo 0-3     > /dev/cpuset/system-background/cpus
-	echo 4-7     > /dev/cpuset/foreground/boost/cpus
 	echo 0-2,4-7 > /dev/cpuset/foreground/cpus
 	echo 0-7     > /dev/cpuset/top-app/cpus
 
