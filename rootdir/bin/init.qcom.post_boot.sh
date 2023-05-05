336cf539a880958adbeecebb03cd20824914d736
Pranav Vashi <neobuddy89@gmail.com>
2023-05-06 00:59:34 +0530

    sm8150-common: rootdir: Set cpuset for restricted
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -67,6 +67,7 @@ case "$target" in
 	# cpuset parameters
 	echo 0-1     > /dev/cpuset/background/cpus
 	echo 0-3     > /dev/cpuset/system-background/cpus
+	echo 0-3     > /dev/cpuset/restricted/cpus
 	echo 0-2,4-7 > /dev/cpuset/foreground/cpus
 	echo 0-7     > /dev/cpuset/top-app/cpus
 
