b24cca757ca6f3263de215191758dd733506d7f4
Bharath <bharath@teamb58.org>
2023-05-21 13:03:24 +0530

    sm8150-common: Switch to QTI Thermal HAL
    Thermal HAL 2.0 has support for a lot of additional
    features including temperature protection and others.
    
    Also, Thermal HAL 1.0 is not supported in FCM level 4.
    
    Change-Id: I1f4fd1957a776934cf5a85e214caf01bd73de47c
    (cherry picked from commit dd83877166d7e43c6d2e3eb1a0c2fccbeec28f48)
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -475,6 +475,10 @@ PRODUCT_BOOT_JARS += \
     telephony-ext \
     xiaomi-telephony-stub
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb
