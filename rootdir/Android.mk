e0059696ea57264da04050c5c38869241741f5ee
Albert I <kras@raphielgang.org>
2023-07-23 16:52:53 +0200

    sm8150-common: Add support for virtual A/B configuration
    * fstab_AB.qcom and partition size values are from
      nabu_global-user 12 RKQ1.200826.002 V13.1.4.0.SKXMIXM release-keys
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Change-Id: I3f6d26bbfd6c8f72c0f5621f83cddb2b4f69b1a3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -5,12 +5,17 @@ LOCAL_MODULE       := fstab.qcom_ramdisk
 LOCAL_MODULE_STEM  := fstab.qcom
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
+ifeq ($(TARGET_IS_VAB),true)
+LOCAL_SRC_FILES    := etc/fstab_AB.qcom
+LOCAL_MODULE_PATH  := $(TARGET_VENDOR_RAMDISK_OUT)/first_stage_ramdisk
+else
 ifneq ($(TARGET_IS_LEGACY),true)
 LOCAL_SRC_FILES    := etc/fstab_dynamic.qcom
 else
 LOCAL_SRC_FILES    := etc/fstab_legacy.qcom
 endif
 LOCAL_MODULE_PATH  := $(TARGET_RAMDISK_OUT)
+endif
 include $(BUILD_PREBUILT)
 
 # prebuilt_root doesn't cover recovery ramdisk, that sucks
