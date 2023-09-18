9d2c099eef10480bffb17ea08e3d648ab5418b61
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    sm8150-common: parts: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraSettingsActivity.java
@@ -20,7 +20,6 @@ package org.lineageos.settings.popupcamera;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
 
@@ -32,8 +31,8 @@ public class PopupCameraSettingsActivity extends CollapsingToolbarBaseActivity {
 
         getFragmentManager()
                 .beginTransaction()
-                .replace(R.id.content_frame, new PopupCameraSettingsFragment(),
-                        TAG_POPUPCAMERA)
+                .replace(com.android.settingslib.collapsingtoolbar.R.id.content_frame,
+                        new PopupCameraSettingsFragment(), TAG_POPUPCAMERA)
                 .commit();
     }
 }
