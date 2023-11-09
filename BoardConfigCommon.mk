3a816f563a5366627d6136ebd32177f018058c9a
Pranav Vashi <neobuddy89@gmail.com>
2023-11-09 22:31:53 +0530

    sm8150-common: Switch to latest available clang
    Change-Id: I2696860e7b9a7d16bd3740d334009c4a1f878aef
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -115,6 +115,7 @@ TARGET_KERNEL_CONFIG := \
     vendor/sm8150-perf_defconfig \
     vendor/xiaomi/sm8150-common.config \
     vendor/debugfs.config
+TARGET_KERNEL_CLANG_VERSION := latest
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
