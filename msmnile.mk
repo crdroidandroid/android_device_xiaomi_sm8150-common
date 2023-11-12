a19ffc6efcb6322da857b18add8c6820852c4a09
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-12 19:44:29 +0100

    sm8150-common: Move to health AIDL services
    Change-Id: Ibadf22d432791551c16d7f4e386b470574c390f8

--- msmnile.mk
+++ msmnile.mk
@@ -204,9 +204,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.example \
+    android.hardware.health-service.example_recovery
 
 # HIDL
 PRODUCT_PACKAGES += \
