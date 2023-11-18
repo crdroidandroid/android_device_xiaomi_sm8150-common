3a357b9826d65c4c3497d24eb37e603468def5c3
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-11-18 19:37:38 +0100

    sm8150-common: Move to Xiaomi IR HIDL service
    Change-Id: I5717475052c89fd696e3fd544e39698450e28fd7

--- msmnile.mk
+++ msmnile.mk
@@ -123,8 +123,7 @@ PRODUCT_PACKAGES += \
 
 # Consumer IR
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
+    android.hardware.ir@1.0-service.xiaomi
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
