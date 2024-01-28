4eded03cd8ef6f515ac03b7521510b68b7dadb6b
Pranav Vashi <neobuddy89@gmail.com>
2024-01-28 17:56:05 +0530

    sm8150-common: Remove gsi developer keys include
    Change-Id: Ifabc52e260a6ae2cd1fea920f29a714ec194e0b0
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -22,9 +22,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
