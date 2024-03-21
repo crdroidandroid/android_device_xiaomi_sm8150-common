05da1635fe06e7b4bc85f2035e869355355d20fd
Michael Bestas <mkbestas@lineageos.org>
2024-03-21 13:38:44 +0200

    sm8150-common: Switch to android.hardware.usb@1.3-service.dual_role_usb
    Change-Id: Ibd14f7d31dfaf41e040302eda3bc49f351deb618
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -488,7 +488,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.3-service.dual_role_usb
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
