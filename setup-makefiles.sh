79855111c69f8de0cbf38618b058c25f3d4ff022
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-18 09:52:31 +0300

    sm8150-common: Skip dependencies which aren't defined in .bp
    Change-Id: I01a653661856495788c05b720c0577dac44998a4
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -47,6 +47,11 @@ function lib_to_package_fixup_vendor_variants() {
             vendor.qti.imsrtpservice@3.0)
             echo "${1}_vendor"
             ;;
+        libOmxCore | \
+            libgrallocutils | \
+            libwpa_client)
+            # Android.mk only packages
+            ;;
         *)
             return 1
             ;;
