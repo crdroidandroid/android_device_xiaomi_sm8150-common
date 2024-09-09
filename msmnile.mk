03a6a83340120d2b67c929fffb1ba24000062992
LuK1337 <priv.luk@gmail.com>
2024-09-09 10:32:07 +0200

    sm8150-common: Import media codecs configuration from vendor
    neobuddy89:
    * Merge media_codecs_system_default into media_codecs_vendor, drop unavailable lahaina imports.
    
    Change-Id: I97eaa5ba603a951077f650649000fbbe35715b3e
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- msmnile.mk
+++ msmnile.mk
@@ -324,6 +324,9 @@ PRODUCT_PACKAGES += \
     libstagefright_foundation-v33
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media/media_codecs_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_vendor.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_c2_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_c2_audio.xml \
