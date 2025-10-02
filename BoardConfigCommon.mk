be54e0100cd32658250c8373c9b50800443d9a71
Pranav Vashi <neobuddy89@gmail.com>
2025-10-02 10:48:50 +0530

    sm8150-common: Include product props
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -162,6 +162,7 @@ TARGET_BOARD_PLATFORM := msmnile
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 ifneq ($(TARGET_IS_TABLET),true)
