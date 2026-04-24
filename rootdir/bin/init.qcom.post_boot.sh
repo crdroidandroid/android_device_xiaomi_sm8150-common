72d33ee0fc643e6f17508a2503c63396e6fd53a5
Pranav Vashi <neobuddy89@gmail.com>
2026-04-24 07:59:20 +0530

    sm8150-common: init: Update swappiness and watermark threshold
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -656,12 +656,12 @@ function configure_memory_parameters() {
     # Set allocstall_threshold to 0 for all targets.
     echo 0 > /sys/module/vmpressure/parameters/allocstall_threshold
 
-    # Set swappiness to 60 for all targets
-    echo 60 > /proc/sys/vm/swappiness
+    # Set swappiness to 100 for all targets
+    echo 100 > /proc/sys/vm/swappiness
 
     # Disable wsf for all targets beacause we are using efk.
     # wsf Range : 1..1000 So set to bare minimum value 1.
-    echo 1 > /proc/sys/vm/watermark_scale_factor
+    echo 120 > /proc/sys/vm/watermark_scale_factor
 
     # Disable the feature of watermark boost for 8G and below device
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
